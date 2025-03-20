9b8b52c53fee41f8e1d7e64242706d6f40afe2d5
HafeezUllah886 <Hafeezullah886@gmail.com>
2025-03-20 13:02:53 +0500

    Initial commit

---
+++ routes/targets.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\SelfTargetController;
+use App\Http\Controllers\TargetsController;
+use App\Http\Middleware\adminCheck;
+use App\Http\Middleware\confirmPassword;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth', adminCheck::class)->group(function () {
+
+    Route::resource('targets', TargetsController::class);
+    Route::get('target/delete/{id}', [TargetsController::class, 'destroy'])->name('target.delete')->middleware(confirmPassword::class);
+
+    Route::resource('self_targets', SelfTargetController::class);
+    Route::get('self_target/delete/{id}', [SelfTargetController::class, 'destroy'])->name('self_target.delete')->middleware(confirmPassword::class);
+    Route::get("selftarger/getcat/{id}", [SelfTargetController::class, 'getcat']);
+
+});
+
